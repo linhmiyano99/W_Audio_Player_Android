668e0fdd62729951e483b678a9858ccff9a673bb
LongKelvin <longkelvin101099@gmail.com>
2019-12-17 21:50:53 +0700

    Fix UI problem
    fix some problem occurs by UI

--- app/src/main/java/com/e/w_audio_player/RecognizeSongsActivity.java
+++ app/src/main/java/com/e/w_audio_player/RecognizeSongsActivity.java
@@ -1,6 +1,7 @@
 package com.e.w_audio_player;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,6 +11,11 @@ public class RecognizeSongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recognize_songs);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
     }
 }
